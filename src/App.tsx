532cad2d8fe39db7a08e2e56db9c42e728ad9f10
Adeeb Abdul Salam <adeebnabdulsalam@gmail.com>
2025-08-16 15:25:57 +0400

    custom chatbot with n8n

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Services from "./components/Services";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ChatBot from "./components/ChatBot";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Team />
       <Contact />
       <Footer />
+      <ChatBot />
     </>
   );
 };
